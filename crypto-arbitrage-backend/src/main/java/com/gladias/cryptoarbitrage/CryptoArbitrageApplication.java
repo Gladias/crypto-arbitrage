9815aa2031880a64ed8ac2d216fe8bd142170654
Gladias <alek.drozdz@gmail.com>
2024-05-11 17:24:37 +0200

    Init BE

---
+++ crypto-arbitrage-backend/src/main/java/com/gladias/cryptoarbitrage/CryptoArbitrageApplication.java
@@ -0,0 +1,12 @@
+package com.gladias.cryptoarbitrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoArbitrageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoArbitrageApplication.class, args);
+    }
+}
---
+++ crypto-arbitrage-backend/src/test/java/com/gladias/cryptoarbitrage/CryptoArbitrageApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gladias.cryptoarbitrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoArbitrageApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
